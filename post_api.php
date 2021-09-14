88e5778ac39bcf13f7a9246c3b9a29f6519d0a38
Hemraj Ramnarine <hemraj@scggrowthpartners.com>
2021-09-14 15:20:11 -0400

    Updated POST

--- post_api.php
+++ post_api.php
@@ -1,9 +1,17 @@
 <?php
 header("Content-Type:application/json");
-if (isset($_POST['leaddata']) && $_POST['leaddata']!="") {
+/*if (isset($_POST['leaddata']) && $_POST['leaddata']!="") {
 	$leaddata = $_POST['leaddata'];
 	echo $leaddata;
 	error_log(print_r($leaddata, true));
 }
-error_log(print_r("No data", true));
+error_log(print_r("No data", true));*/
+
+// takes raw data from the request 
+$json = file_get_contents('php://input');
+// Converts it into a PHP object 
+$data = json_decode($json, true);
+ echo $data;
+error_log(print_r($data, true));
+
     ?>
